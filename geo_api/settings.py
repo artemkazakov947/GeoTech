e11270971b8683a6e674fcca498531e920599c23
Artem Kazakov <artemkazakov947@gmail.com>
2023-05-31 16:45:07 +0200

    Postgis DB was connected

--- geo_api/settings.py
+++ geo_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
     "places"
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "geo_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "geotech",
+        "USER": "admin",
+        "PASSWORD": "admin12345",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -123,3 +128,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GDAL_LIBRARY_PATH = "/opt/homebrew/lib/libgdal.dylib"
+GEOS_LIBRARY_PATH = "/opt/homebrew/lib/libgeos_c.dylib"
