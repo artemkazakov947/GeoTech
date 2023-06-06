093ec5b883c62ed7d84fcc5d98f879d914d0452d
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-06 17:32:30 +0200

    Added unique_together validation to database level

--- places/models.py
+++ places/models.py
@@ -4,7 +4,13 @@ from django.contrib.gis.db import models
 class Place(models.Model):
     name = models.CharField(max_length=512)
     description = models.TextField()
-    geom = models.PointField(blank=True, null=True)
+    geom = models.PointField(blank=True, null=True, unique=True)
+
+    class Meta:
+        unique_together = (
+            "name",
+            "geom"
+        )
 
     def __str__(self):
         return f"{self.name}"
