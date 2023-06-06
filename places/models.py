ba44e27e7923ea4a00375e4df5e1cf0b5ac92d10
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-06 12:01:52 +0200

    Changed field "geom" in Place model

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from django.contrib.gis.db import models
 class Place(models.Model):
     name = models.CharField(max_length=512)
     description = models.TextField()
-    geom = models.PointField()
+    geom = models.PointField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.name} : {self.geom.wkt}"
+        return f"{self.name}"
