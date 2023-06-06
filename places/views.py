3425123feb0846fe7616e175dee4b2ceb6b0903b
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-06 13:02:40 +0200

    Added swagger documentation

--- places/views.py
+++ places/views.py
@@ -1,5 +1,7 @@
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.request import Request
@@ -13,8 +15,44 @@ class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
+    def list(self, request, *args, **kwargs) -> Response:
+        """List for all places"""
+        return super().list(request)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="long",
+                type=OpenApiTypes.DECIMAL,
+                description="point's longitude",
+                examples=[
+                    OpenApiExample(
+                        "Example",
+                        description="provide longitude for point",
+                        value="4.34878",
+                    ),
+                ],
+            ),
+            OpenApiParameter(
+                name="lat",
+                type=OpenApiTypes.DECIMAL,
+                description="point's latitude",
+                examples=[
+                    OpenApiExample(
+                        "Example",
+                        description="provide latitude for point",
+                        value="50.85045",
+                    ),
+                ],
+            ),
+        ],
+        examples=[
+            OpenApiExample("Example 1", description="Find nearest place to point", value="?long=4.34878&lat=50.85045"),
+        ],
+    )
     @action(methods=["GET"], url_path="nearest_place", detail=False)
     def nearest_place(self, request: Request) -> Response:
+        """Endpoint which provide nearest place to given point (point gives by get params)"""
         long = request.GET.get("long", 0)
         lat = request.GET.get("lat", 0)
         if long and lat:
